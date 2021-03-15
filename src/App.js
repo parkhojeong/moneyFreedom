fc7c2b29ef5f79f17b2b8b9d91d00ef48742674d
parkhojeong <parkhj062@gmail.com>
2021-03-15 22:13:32 +0900

    미사용 코드 정리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import { Counter } from "./features/counter/Counter";
 import Calculator from "./features/calculator/Calculator";
 import EstimateGoal from "./features/calculator/EstimateGoal";
 import "./App.css";
@@ -8,8 +7,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        {/* <Counter /> */}
         <Calculator />
         <EstimateGoal />
       </header>
