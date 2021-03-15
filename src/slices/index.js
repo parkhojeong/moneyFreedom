7ea80cae4abb1aa77d6560ed357349cdabbf2598
parkhojeong <parkhj062@gmail.com>
2021-03-15 23:13:42 +0900

    제거된 코드 반영

--- src/slices/index.js
+++ src/slices/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from "redux";
 
-import counterReducer from "../features/counter/counterSlice";
 import calculatorReducer from "../features/calculator/calculatorSlice";
 
 const rootReducer = combineReducers({
-  counter: counterReducer,
   calculator: calculatorReducer,
 });
 
